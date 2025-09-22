bc16b60a1917676cfd062f6ea7436feb767ca4bb
carlos <carlos.alb19@icloud.com>
2025-09-22 19:55:05 -0300

    Resolvendo conflitos de merge

--- curso_em_video/ex079.py
+++ curso_em_video/ex079.py
@@ -1,18 +1,18 @@
-valores = []
+valores = [] #lista vazia
 while True:
     nun = int(input('Digite um valor: '))
 
-    if nun in valores:
+    if nun in valores: # verifica se o numero ta na lista
         print('Valor duplicado na lista, Não vou adicionar...')
     else:
-        valores.append(nun)
+        valores.append(nun) # adiciona numero na lista
         print('Valor adicionado com sucesso...')
     continuar = ' '
-    while continuar not in 'SN':
+    while continuar not in 'SN': # pergunta se quer continuar, nao aceita outro caracter
         continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if continuar == 'N':
         break
-valores.sort()
+valores.sort() # ordena a liata de forma crecente 
 print('-=' * 30)
 print(f'Valores cadastrados {valores}')
 
---
+++ curso_em_video/ex080.py
@@ -0,0 +1,17 @@
+lista = []
+for  c in range(0, 5):
+    num = int(input('Digite um valor: '))
+    if c == 0 or num > lista[-1]:
+        lista.append(num)
+        print('Valor adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valore digitados em ordem foram: {lista}')
+
